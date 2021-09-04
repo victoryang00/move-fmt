17b4aa43d551c3a11e343e71f1b41ef99f1b7227
victoryang00 <yangyiwei2000@gmail.com>
2021-09-04 11:59:03 -0700

    fix path check

--- tests/test.rs
+++ tests/test.rs
@@ -1,7 +1,9 @@
+#![feature(path_try_exists)]
 extern crate move_fmt;
 
 use move_fmt::{PestResult, Settings, grammar::{Rule, MoveParser}};
 use pest::{parses_to, consumes_to};
+use std::fs;
 
 #[test]
 fn test_address() {
@@ -19,11 +21,19 @@ fn test_address() {
 #[test]
 fn test_move() -> PestResult<()> {
     let mut cfg = Settings::default();
+    match  fs::try_exists("tests/out/") {
+       Ok(false)=> fs::create_dir("tests/out/"),
+        _ => Ok(())
+    };
     cfg.format_file("tests/td.move", "tests/out/td.move")
 }
 
 #[test]
 fn test_mvir() -> PestResult<()> {
     let mut cfg = Settings::default();
+    match  fs::try_exists("tests/out/") {
+        Ok(false)=> fs::create_dir("tests/out/"),
+        _ => Ok(())
+    };
     cfg.format_file("tests/td.mvir", "tests/out/td.mvir")
 }
